e7539ba81ca785fac21477fb2a2c2a4bd9579a3b
avinanick <avinanick@gmail.com>
2015-10-21 12:56:36 -0700

    first commit

---
+++ src/main/java/com/mycompany/firstmavenproject/MainClass.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.firstmavenproject;
+
+/**
+ *
+ * @author nick.avina346
+ */
+public class MainClass {
+    public static void main (String[] Args) {
+        System.out.println("We're working with maven!");
+    }
+}
